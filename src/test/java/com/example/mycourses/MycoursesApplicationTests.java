e16306d86bcad302b47efc29518db0f049d4f7cb
itsabunny <itsabunny@pm.me>
2025-06-11 15:20:12 +0200

    initialize project with Spring Boot and Maven Wrapper

---
+++ src/test/java/com/example/mycourses/MycoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
